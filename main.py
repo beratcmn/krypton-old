ce23f82d3e7c10cacb1c51ee9cac0dce9207b078
Berat Çimen <beratcmn@gmail.com>
2021-11-16 02:17:20 +0300

    Her şeyi RegEx ile yaptım ve while ekledim

--- main.py
+++ main.py
@@ -1,153 +1,109 @@
 import os
 import sys
+import re
 
 extension = ".kr"
 
 
-def ListToString(_list: list):
+def EvalLine(InputLine: str):
+    FinalLine = re.sub("    ", "/tab/", InputLine)
 
-    string = ""
+    # print
+    print_matches = re.findall('yazdır\("*.*"*\)', FinalLine)
+    if len(print_matches) > 0:
+        # print_matches[0].replace("yazdır", "print")
+        FinalLine = re.sub("yazdır\(", "print(", FinalLine)
 
-    for char in _list:
-        string += char
-
-    return string
-
-
-def ArrangeTabs(_line: str):
-    _list = list(_line)
-    # print(_list)
-
-    for i in _list:
-        if _list.index(i) == 0 and i == "?":
-            _list[_list.index(i)] = "    "
-        elif _list[_list.index(i) - 1] == "    " and i == "?":
-            _list[_list.index(i)] = "    "
-
-    return ListToString(_list)
-
-
-def EvalLine(_line: str):
-    line = ""
-    _line = _line.replace("    ", "?")
-
-    #
-    if _line[:8] == "değişken":
-        temp_list = list(_line)
-        temp_list[:8] = ""
-        if "=" in _line:
-            line = ListToString(temp_list)
-        else:
-            line = str(ListToString(temp_list)).strip()
-            temp_list = list(line)
-            temp_list[:len(temp_list)] = temp_list[:len(
-                temp_list)][0] + " = None"
-            line = ListToString(temp_list)
-
-    # print ()
-    if _line[:6] == "yazdır":
-        temp_list = list(_line)
-        temp_list[:6] = "print"
-
-        line = ListToString(temp_list)
+    # variable decleration
+    var_matches = re.findall('değişken .* = .*', FinalLine)
+    if len(var_matches) == 0:
+        var_matches2 = re.findall('değişken .*', FinalLine)
+        if len(var_matches2) > 0:
+            FinalLine = re.sub("değişken ", "", FinalLine) + " = None"
+    else:
+        FinalLine = str(re.sub("değişken ", "", FinalLine))
 
     # if
-    if _line[:4] == "eğer":
-        temp_list = list(_line)
-        temp_list[:4] = "if"
-        temp_list.append(":")
-
-        line = ListToString(temp_list)
-        line = line.replace(" ve ", " and ")
-        line = line.replace(" veya ", " or ")
-
-    # else
-    if _line[:7] == "değilse":
-        temp_list = list(_line)
-        temp_list[:7] = "else"
-        temp_list.append(":")
-
-        line = ListToString(temp_list)
+    if_matches = re.findall('eğer .*', FinalLine)
+    if len(if_matches) > 0:
+        FinalLine = re.sub("eğer ", "if ", FinalLine, 1) + ":"
 
     # elif
-    if _line[:10] == "değilse ve":
-        temp_list = list(_line)
-        temp_list[:10] = "elif"
-        temp_list.append(":")
+    elif_matches = re.findall('değilse ve .*', FinalLine)
+    if len(elif_matches) > 0:
+        FinalLine = re.sub("değilse ve ", "elif ", FinalLine, 1) + ":"
 
-        line = ListToString(temp_list)
-        line = line.replace(" ve ", " and ")
-        line = line.replace(" veya ", " or ")
+    # else
+    else_matches = re.findall('değilse ?', FinalLine)
+    if len(else_matches) > 0:
+        FinalLine = re.sub("değilse ?", "else", FinalLine) + ":"
+
+    # def fonksiyon
+    def_matches = re.findall('fonksiyon .*\(.*\)', FinalLine)
+    if len(def_matches) > 0:
+        FinalLine = re.sub("fonksiyon ", "def ", FinalLine) + ":"
+    else:
+        def_matches2 = re.findall('f .*\(.*\)', FinalLine)
+        if len(def_matches2) > 0:
+            FinalLine = re.sub("f ", "def ", FinalLine, 1) + ":"
 
     # for
-    if "içindeki herbir" in _line and "için" in _line:
-        temp_list = _line.split()
-        if (temp_list.index("içindeki") == 1) and (temp_list.index("herbir") == 2) and (temp_list.index("için") == 4):
-            temp_list.remove("içindeki")
-            temp_list.remove("herbir")
-            temp_list.remove("için")
+    for_matches = re.findall('.* içindeki herbir .* için', FinalLine)
+    if len(for_matches) > 0:
+        for_tab_count = len(re.findall('\/tab\/', FinalLine))
+        FinalLine = re.sub("\/tab\/", "", FinalLine)
+        FinalLine = re.sub(" içindeki herbir", "", FinalLine, 1)
+        FinalLine = re.sub(" için", "", FinalLine, 1)
+        FinalLine = re.split('\s+', FinalLine)
+        FinalLine = for_tab_count * "/tab/" + "for " + \
+            FinalLine[1] + " in " + FinalLine[0] + ":"
 
-        line = "for " + str(temp_list[1]) + " in " + str(temp_list[0]) + ":"
-
-    # def
-    if _line[:9] == "fonksiyon":
-        temp_list = list(_line)
-        temp_list[:9] = "def"
-        temp_list.append(":")
-
-        line = ListToString(temp_list)
-    elif _line[:1] == "f" and _line[1:2] == " ":
-        temp_list = list(_line)
-        temp_list[:1] = "def"
-        temp_list.append(":")
-
-        line = ListToString(temp_list)
+    # while
+    while_matches = re.findall('.* olduğu sürece', FinalLine)
+    if len(while_matches) > 0:
+        while_tab_count = len(re.findall('\/tab\/', FinalLine))
+        FinalLine = re.sub("\/tab\/", "", FinalLine)
+        FinalLine = while_tab_count * "/tab/" + "while " + \
+            re.sub(" olduğu sürece", "", FinalLine, 1) + ":"
 
     # return
-    if _line[:6] == "döndür":
-        temp_list = list(_line)
-        temp_list[:6] = "return"
-
-        line = ListToString(temp_list)
-
-    # "    "
-    if _line[:1] == "?":
-        temp_list = list(_line)
-        temp_list[:1] = ""
-        temp_line = ListToString(temp_list)
-        line = "?" + EvalLine(temp_line)
+    return_matches = re.findall('döndür .*', FinalLine)
+    if len(return_matches) > 0:
+        FinalLine = re.sub("döndür ", "return ", FinalLine, 1)
 
     # ^2
-    if "kare(" in _line:
-        _line = _line.replace("kare(", "mithen.kare(")
-        line = _line
+    square_matches = re.findall('kare\(.*?\)', FinalLine)
+    if len(square_matches) > 0:
+        FinalLine = re.sub("kare\(", "mithen.kare(", FinalLine)
 
     # ^3
-    if "küp(" in _line:
-        _line = _line.replace("küp(", "mithen.kup(")
-        line = _line
+    cube_matches = re.findall('küp\(.*?\)', FinalLine)
+    if len(cube_matches) > 0:
+        FinalLine = re.sub("küp\(", "mithen.kup(", FinalLine)
 
     # ^1/2
-    if "karekök(" in _line:
-        _line = _line.replace("karekök(", "mithen.karekok(")
-        line = _line
+    squareroot_matches = re.findall('karekök\(.*?\)', FinalLine)
+    if len(squareroot_matches) > 0:
+        FinalLine = re.sub("karekök\(", "mithen.karekok(", FinalLine)
 
     # ^1/3
-    if "küpkök(" in _line:
-        _line = _line.replace("küpkök(", "mithen.kupkok(")
-        line = _line
+    cuberoot_matches = re.findall('küpkök\(.*?\)', FinalLine)
+    if len(cuberoot_matches) > 0:
+        FinalLine = re.sub("küpkök\(", "mithen.kupkok(", FinalLine)
 
-    # Absolute
-    if "mutlak(" in _line:
-        _line = _line.replace("mutlak(", "mithen.mutlak(")
-        line = _line
+    # absolute
+    abs_matches = re.findall('mutlak\(.*?\)', FinalLine)
+    if len(abs_matches) > 0:
+        FinalLine = re.sub("mutlak\(", "mithen.mutlak(", FinalLine)
 
     # time.sleep()
-    if "bekle(" in _line:
-        _line = _line.replace("bekle(", "mithen.bekle(")
-        line = _line
+    timesleep_matches = re.findall('bekle\(.*?\)', FinalLine)
+    if len(timesleep_matches) > 0:
+        FinalLine = re.sub("bekle\(", "mithen.bekle(", FinalLine)
 
-    return ArrangeTabs(line.strip())
+    # Final return
+    FinalLine = re.sub("\/tab\/", "    ", FinalLine)
+    return FinalLine
 
 
 def Compile(_filename: str, _run: bool):
@@ -165,10 +121,11 @@ def Compile(_filename: str, _run: bool):
     for _il in inputLines:
         # because of the dynamic declaring any python code will be valid in krypton.
         # ? another aproach for the defs would be adding them to a global list then interpreting them.
-        newLine = EvalLine(_il) if EvalLine(_il) != "" else _il
+        #_el = EvalLine(_il)
+        #newLine = _el if _el != "" else _il
         # delete ( and newLine != "") if you want an exact translation in terms of spaces and line breaks.
-        if newLine != None and newLine != "":
-            outputLines.append(newLine)
+        # if newLine != None and newLine != "":
+        outputLines.append(EvalLine(_il))
 
     outputFile = open(filename_output, "w+", encoding="utf-8")
     for line in outputLines:
